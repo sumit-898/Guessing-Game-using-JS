17747925bf713cb7aa135167f08b9d4479312186
Sumit kumar <sumitkumartiwari8521@gmail.com>
2024-07-29 22:26:57 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const max=prompt("Enter max number");
+
+const random=Math.floor(Math.random()*max)+1;
+console.log(random);
+
+let guess=prompt("guess the number");
+
+while(true){
+    if(guess == "quit"){
+        console.log("User quit");
+        break;
+    }
+
+    if(guess == random){
+        console.log("You are right! congrats!! random number was",random);
+        break;
+    }
+    else if(guess<random){
+        guess= prompt("Hint:Your guess was to small. Please try again");
+    }else{
+        guess= prompt("Hint:Your guess was to large. Please try again");
+    }
+
+}
